09fa8db94a403ec9143b7e5cde75f4ff6f6854f0
root <root@ip-172-31-46-183.ec2.internal>
2019-07-08 09:33:51 +0000

    kodak

---
+++ src/main/java/com/kodak/App.java
@@ -0,0 +1,13 @@
+package com.kodak;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
